eae61fb34dc5d9fe7a1e5962733596b5a94bbbea
Dzianis Haivaronski <gajvoronski@gmail.com>
2022-03-24 13:04:45 +0300

    Generator .psv files for gainwell/incidents

---
+++ gainwell_insidents.py
@@ -0,0 +1,39 @@
+from itertools import cycle
+import string
+import random
+
+pool = cycle(['I', 'E', 'R', 'C'])
+number_of_lines = 10
+
+def generate_random_string(length):
+    """Функция для случайной строки"""
+    letters = string.ascii_lowercase
+    rand_string = ''.join(random.choice(letters) for i in range(length))
+    return rand_string
+
+with open('PERFORMANCE_TestPenrollIcPrIncident_18838_WI_T_PR_APPLN_20210101-20211019.psv', 'w') as f:
+    f.write('SAK_ATN|DTE_RECEIVED|DTE_FINALIZED|CDE_PROV_TYPE|CDE_ENROLL_TYPE')
+
+    DTE_RECEIVED = '20210101'
+    DTE_FINALIZED = '20210101'
+    CDE_PROV_TYPE = (i for i in range(500000))
+    CDE_ENROLL_TYPE = (i for i in pool)
+
+    for line in range(number_of_lines):
+        SAK_ATN = 'PERFORMANCE_' + generate_random_string(41)
+        s_out = '\n' + SAK_ATN + '|' + DTE_RECEIVED + '|' + DTE_FINALIZED + '|' + str(next(CDE_PROV_TYPE)) + '|' + next(CDE_ENROLL_TYPE)
+        f.write(s_out)
+
+
+with open('PERFORMANCE_TestPenrollIcPrIncident_18838_WI_T_PR_TYPE_CDE_20210101-20211019.psv', 'w') as f:
+    f.write('CDE_PROV_TYPE|DSC_PROV_TYPE')
+
+    CDE_PROV_TYPE = (i for i in range(500000))
+
+    for line in range(number_of_lines):
+        DSC_PROV_TYPE = generate_random_string(15)
+        s_out = '\n' + str(next(CDE_PROV_TYPE)) + '|' +  DSC_PROV_TYPE
+        f.write(s_out)
+
+
+
